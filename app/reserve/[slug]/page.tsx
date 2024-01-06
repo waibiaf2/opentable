360a809dc28863f19e609607c707527fb9cb9349
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-01-06 16:16:16 +0300

    Refactor reservation section into sub components

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/app/components/NavBar";
 import Header from "@/app/reserve/[slug]/components/Header";
+import Form from "@/app/reserve/[slug]/components/Form";
 
 const Reserve = () => {
 	return (
@@ -9,54 +10,11 @@ const Reserve = () => {
 				<div className="border-t h-screen">
 					<div className="py-9 w-3/5 m-auto">
 						<Header/>
-						{/* FORM */}
-						<div className="mt-10 flex flex-wrap justify-between w-[660px]">
-							<input
-								type="text"
-								className="border rounded p-3 w-80 mb-4"
-								placeholder="First name"
-							/>
-							<input
-								type="text"
-								className="border rounded p-3 w-80 mb-4"
-								placeholder="Last name"
-							/>
-							<input
-								type="text"
-								className="border rounded p-3 w-80 mb-4"
-								placeholder="Phone number"
-							/>
-							<input
-								type="text"
-								className="border rounded p-3 w-80 mb-4"
-								placeholder="Email"
-							/>
-							<input
-								type="text"
-								className="border rounded p-3 w-80 mb-4"
-								placeholder="Occasion (optional)"
-							/>
-							<input
-								type="text"
-								className="border rounded p-3 w-80 mb-4"
-								placeholder="Requests (optional)"
-							/>
-							<button
-								className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
-							>
-								Complete reservation
-							</button>
-							<p className="mt-4 text-sm">
-								By clicking “Complete reservation” you agree to the OpenTable Terms
-								of Use and Privacy Policy. Standard text message rates may apply.
-								You may opt out of receiving text messages at any time.
-							</p>
-						</div>
+						<Form/>
 					</div>
 				</div>
 			</main>
 		</main>
-	
 	);
 };
 
