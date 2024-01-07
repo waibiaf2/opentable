ff00ab72bf9eb7cef796fb9f36b1394122f98e2c
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-01-08 02:53:39 +0300

    add dynamic data fetching to home page.

---
+++ app/components/Price.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {PRICE} from "@prisma/client";
+
+const Price = ({price}:{price : PRICE}) => {
+	const renderPrice = () => {
+		if (price === PRICE.CHEAP) {
+			return <>
+				<span>$$</span><span className="text-gray-400">$$</span>
+			</>
+		}else if (price === PRICE.REGULAR) {
+			return <>
+				<span>$$$</span><span className="text-gray-400">$</span>
+			</>
+		}else if (price === PRICE.EXPENSIVE) {
+			return <>
+				<span>$$$$</span>
+			</>
+		}
+	}
+	return (
+		<p className="flex mr-3">
+			{renderPrice()}
+		</p>
+	);
+};
+
+export default Price;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,46 @@
 import Header from "@/app/components/Header";
 import RestaurantCard from "@/app/components/RestaurantCard";
+import {Cuisine, Location, PRICE, PrismaClient} from "@prisma/client";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export interface RestaurantCardType {
+	id: number,
+	name: string,
+	main_image: string,
+	cuisine: Cuisine,
+	location: Location,
+	price: PRICE,
+	slug: string
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+	return prisma.restaurant.findMany({
+		select: {
+			id: true,
+			name: true,
+			main_image: true,
+			cuisine: true,
+			location: true,
+			price: true,
+			slug: true
+		}
+	});
+}
+
+export default async function Home() {
+	const restaurants = await fetchRestaurants();
+	// console.log({restaurants})
+	
 	return (
-		<>
-			<main>
-				<Header/>
-				<div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-					<RestaurantCard/>
-				</div>
-			</main>
-		</>
+		<main>
+			<Header/>
+			<div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+				{restaurants.map((restaurant) => (
+					<RestaurantCard key={restaurant.id} restaurant={restaurant}/>
+				))}
+			</div>
+		</main>
 	);
 }
 
